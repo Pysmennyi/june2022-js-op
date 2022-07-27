d322658ebc90957caa0db8a06e848b847c22818b
Oleksii Pysmennyi <pismennyia2017@ukr.net>
2022-07-27 10:17:46 +0300

    HomeWorkForLesson2IsDone

---
+++ hw/lesson2/hw2.js
@@ -0,0 +1,119 @@
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+
+console.log(`Solution Task1:`)
+let arr1 = [1, 2, 3, 4, 5, `six`, `seven`, 8, true, false];
+console.log(arr1[0]);
+console.log(arr1[1]);
+console.log(arr1[2]);
+console.log(arr1[3]);
+console.log(arr1[4]);
+console.log(arr1[5]);
+console.log(arr1[6]);
+console.log(arr1[7]);
+console.log(arr1[8]);
+console.log(arr1[9]);
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+let book1 = {
+    title: `501 fact, that need to know from the geography`,
+    pageCount: 256,
+    genre: `science`
+}
+let book2 = {
+    title: `short answers to a great questions`,
+    pageCount: 224,
+    genre: `biography`,
+}
+let book3 = {
+    title: `white fang`,
+    pageCount: 239,
+    genre: `fiction`,
+}
+// - Створити 3 об'єкти які описують книги. Поля об'єкту
+// : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
+let book4 = {
+    title: `501 fact, that need to know from the geography`,
+    pageCount: 256,
+    genre: `science`,
+    authors: [`name`, `age`],
+}
+let book5 = {
+    title: `short answers to a great questions`,
+    pageCount: 224,
+    genre: `biography`,
+    authors: [`name`, `age`],
+}
+let book6 = {
+    title: `white fang`,
+    pageCount: 239,
+    genre: `fiction`,
+    authors: [`name`, `age`],
+}
+// - Створити масив з 10 об'єктами які описують сутніть "користувач".
+// Поля: name, username,password. Вивести в консоль пароль кожного користувача
+
+let arr2 = [
+    user1 = {
+        name: `Olek`,
+        username: `user1`,
+        password: `13213ds`,
+    },
+    user2 = {
+        name: `Tomas`,
+        username: `user2`,
+        password: `dsfsE43`,
+    },
+    user3 = {
+        name: `Natali`,
+        username: `user3`,
+        password: `dj56xgT`,
+    },
+    user4 = {
+        name: `Tania`,
+        username: `user4`,
+        password: `jhgfd882364sdsf`,
+    },
+    user5 = {
+        name: `Tetiana`,
+        username: `user5`,
+        password: 2414323,
+    },
+    user6 = {
+        name: `Kateryna`,
+        username:`user6`,
+        password:`kateryna`,
+    },
+    user7 = {
+        name:`Mariana`,
+        username:`user7`,
+        password:`Warsaw2019`,
+    },
+    user8 = {
+        name:`Iryna`,
+        username:`user8`,
+        password:`GreatBritain23`,
+    },
+    user9 = {
+        name:`Volodymyr`,
+        username:`user9`,
+        password:`president6`,
+    },
+    user10 = {
+        name:`Kaktus`,
+        username:`user10`,
+        password:`kaktus`,
+    }
+
+]
+    console.log(`Solution Task3:`);
+
+console.log(arr2[0].password);
+console.log(arr2[1].password);
+console.log(arr2[2].password);
+console.log(arr2[3].password);
+console.log(arr2[4].password);
+console.log(arr2[5].password);
+console.log(arr2[6].password);
+console.log(arr2[7].password);
+console.log(arr2[8].password);
+console.log(arr2[9].password);
--- hw/lesson_1/hw1.js
+++ hw/lesson_1/hw1.js
@@ -16,105 +16,97 @@
 // let arr = [hello, owu, com, ua, one, ten, nine3, oneTwoThree, pi, twoPSeven, sixteen, bTrue, bFalse];
 //     Вивести кожну змінну за допомогою: console.log , alert, document.write
 
-//VARIANT 1
 
+
+//VARIANT 1
 // for (let i = 0; i < arr.length; i++) {
-    // console.log(arr[i]);
-    // document.write(arr[i] + `, `);
+// console.log(arr[i]);
+// document.write(arr[i] + `, `);
 //     alert(arr[i]);
 // }
-    /*
 //VARIANT 2
-//1
-console.log(hello);
-document.write(hello);
-alert(hello);
-
-//2
-console.log(owu);
-document.write(owu);
-alert(owu);
-*/
-
-/*//3
-console.log(com);
-document.write(com);
-alert(com);
-
-//4
-console.log(ua);
-document.write(ua);
-alert(ua);
-
-//5
-console.log(one);
-document.write(one);
-alert(one);
-
-//6
-console.log(ten);
-document.write(ten);
-alert(ten);
-
-//7
-console.log(nine3);
-document.write(nine3);
-alert(nine3);
-
-//8
-console.log(oneTwoThree);
-document.write(oneTwoThree);
-alert(oneTwoThree);
-
-//9
-console.log(pi);
-document.write(pi);
-alert(pi);
-
-//10
-console.log(twoPSeven);
-document.write(twoPSeven);
-alert(twoPSeven);
-
-//11
-console.log(sixteen);
-document.write(sixteen);
-alert(sixteen);
-
-//12
-console.log(bTrue);
-document.write(bTrue);
-alert(bTrue);
-
-//13
-console.log(bFalse);
-document.write(bFalse);
-alert(bFalse);
-// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
-let firstName;
-let middleName;
-let lastName;
-
-firstName = `Winston`;
-middleName = `Andrijowych`;
-lastName = `Kapszyszak`;
-
-let person = `${firstName} ${middleName} ${lastName}`
-
-console.log(person);
-
-// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
-let a = 100;
-let b = '100';
-let c = true;
-console.log(typeof a);
-console.log(typeof b);
-console.log(typeof c);*/
-
-// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
-
-// let name = prompt('please enter your name');
-// let midName = prompt(`please enter your middle name`);
-// let age = prompt(`please enter your age`);
+// //1
+// console.log(hello);
+// document.write(hello);
+// alert(hello);
 //
-// console.log(`${name} ${midName} ${age} `);
+// //2
+// console.log(owu);
+// document.write(owu);
+// alert(owu);
+//
+// //3
+// console.log(com);
+// document.write(com);
+// alert(com);
+//
+// //4
+// console.log(ua);
+// document.write(ua);
+// alert(ua);
+//
+// //5
+// console.log(one);
+// document.write(one);
+// alert(one);
+//
+// //6
+// console.log(ten);
+// document.write(ten);
+// alert(ten);
+//
+// //7
+// console.log(nine3);
+// document.write(nine3);
+// alert(nine3);
+//
+// //8
+// console.log(oneTwoThree);
+// document.write(oneTwoThree);
+// alert(oneTwoThree);
+//
+// //9
+// console.log(pi);
+// document.write(pi);
+// alert(pi);
+//
+// //10
+// console.log(twoPSeven);
+// document.write(twoPSeven);
+// alert(twoPSeven);
+//
+// //11
+// console.log(sixteen);
+// document.write(sixteen);
+// alert(sixteen);
+//
+// //12
+// console.log(bTrue);
+// document.write(bTrue);
+// alert(bTrue);
+//
+// //13
+// console.log(bFalse);
+// document.write(bFalse);
+// alert(bFalse);
+// // - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+// let firstName;
+// let middleName;
+// let lastName;
+//
+// firstName = `Winston`;
+// middleName = `Andrijowych`;
+// lastName = `Kapszyszak`;
+//
+// let person = `${firstName} ${middleName} ${lastName}`
+//
+// console.log(person);
+//
+// // - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+// let a = 100;
+// let b = '100';
+// let c = true;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
